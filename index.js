9544ef4448a1fa0840f7456b1b53816987ac74b6
Glenn Kamau Mwangi <glennkamau2@gmail.com>
2022-06-17 20:07:38 +0300

    commit tree 07f79408d1ee1888e8f8aa687574a849a675f2c2

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+function reverse(word) {
+  const wordArray = word.split("")
+  const reversedWordArray =wordArray.reverse()
+  const reversedWord = reversedWordArray.join("")
+  return reversedWord;
+  
+}
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  if (word === reversedWord){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 /* 
@@ -17,6 +31,20 @@ if (require.main === module) {
   console.log("=>", isPalindrome("racecar"));
 
   console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ab"));
+
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
